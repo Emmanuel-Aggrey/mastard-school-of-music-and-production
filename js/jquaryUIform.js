883ae8342dd865852d4435c0b2848226c46c9aa5
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-01-23 12:52:33 +0000

    added vue support

--- js/jquaryUIform.js
+++ js/jquaryUIform.js
@@ -1,57 +1,18 @@
 $( function() {
     var dialog, form,
  
-      // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-      // emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-      name = $( "#name" ),
+      fname = $( "#fname" ),
+      oname = $( "#oname" ),
       email = $( "#email" ),
-      password = $( "#qualify" ),
-      allFields = $( [] ).add( name ).add( email ).add( qualify ),
+      qualify = $( "#qualify" ),
       tips = $( ".validateTips" );
  
-    function updateTips( t ) {
-      tips
-        .text( t )
-        .addClass( "ui-state-highlight" );
-      setTimeout(function() {
-        tips.removeClass( "ui-state-highlight", 1500 );
-      }, 500 );
-    }
- 
-    function checkLength( o, n, min, max ) {
-      if ( o.val().length > max || o.val().length < min ) {
-        o.addClass( "ui-state-error" );
-        updateTips( "Length of " + n + " must be between " +
-          min + " and " + max + "." );
-        return false;
-      } else {
-        return true;
-      }
-    }
- 
-    function checkRegexp( o, regexp, n ) {
-      if ( !( regexp.test( o.val() ) ) ) {
-        o.addClass( "ui-state-error" );
-        updateTips( n );
-        return false;
-      } else {
-        return true;
-      }
-    }
- 
+  
+ //not using it yet don't remove
     function addUser() {
-      // var valid = true;
-      // allFields.removeClass( "ui-state-error" );
- 
-      // valid = valid && checkLength( name, "username", 3, 16 );
-      // valid = valid && checkLength( email, "email", 6, 80 );
-      // valid = valid && checkLength( password, "qualify", 5, 16 );
- 
-      // valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-      // valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-      // // valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
- 
-     
+      console.log('email is  ', fname.val())
+      dialog.dialog( "close" );
+
     }
  
     dialog = $( "#dialog-form" ).dialog({
@@ -60,8 +21,8 @@ $( function() {
       width: 500,
       modal: true,
       buttons: {
-        "Create an account": addUser,
-        Cancel: function() {
+      "Apply": addUser,
+      Cancel: function() {
           dialog.dialog( "close" );
         }
       },
@@ -74,6 +35,8 @@ $( function() {
     form = dialog.find( "form" ).on( "submit", function( event ) {
       event.preventDefault();
       addUser();
+     
+
     });
  
     $( "#create-user" ).button().on( "click", function() {
@@ -81,3 +44,11 @@ $( function() {
     });
   } );
  
+
+
+  new Vue({
+    el:'#vueapp',
+    data:{
+      name:'Aggrey is the name'
+    }
+  })
